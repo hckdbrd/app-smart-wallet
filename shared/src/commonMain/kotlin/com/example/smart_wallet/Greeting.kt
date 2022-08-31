527252b19119ede02a374922fcf74b4b42a782dc
hcdkbrd <ennbosh@icloud.com>
2022-08-31 08:58:46 +0300

    Initial commit.

---
+++ shared/src/commonMain/kotlin/com/example/smart_wallet/Greeting.kt
@@ -0,0 +1,7 @@
+package com.example.smart_wallet
+
+class Greeting {
+    fun greeting(): String {
+        return "Hello, ${Platform().platform}!"
+    }
+}
---
+++ shared/src/commonMain/kotlin/com/example/smart_wallet/Platform.kt
@@ -0,0 +1,5 @@
+package com.example.smart_wallet
+
+expect class Platform() {
+    val platform: String
+}
